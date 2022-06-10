6aa9a1db85aa67be9f1b3d9fd9b98fb0d359531c
ø Welcome ø <78008509+hungpurdie@users.noreply.github.com>
2022-06-10 15:25:31 +0700

    Initial commit

---
+++ src/components/Counter/Counter.tsx
@@ -0,0 +1,13 @@
+type CounterProps = {
+  count: number;
+};
+
+function Counter({ count }: CounterProps) {
+  return (
+    <div>
+      <h1>Counter {count}</h1>
+    </div>
+  );
+}
+
+export default Counter;
