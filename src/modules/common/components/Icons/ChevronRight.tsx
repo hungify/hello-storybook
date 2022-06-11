62881b761411f9528d9fc0ee5fab05229c247f04
hungpurdie <nmhung2022@gmail.com>
2022-06-11 23:54:31 +0700

    fix: increase size icon to 24

--- src/modules/common/components/Icons/ChevronRight.tsx
+++ src/modules/common/components/Icons/ChevronRight.tsx
@@ -2,8 +2,8 @@ import { SVGProps } from 'react';
 
 const SvgChevronRight = (props: SVGProps<SVGSVGElement>) => (
   <svg
-    width='1em'
-    height='1em'
+    width={24}
+    height={24}
     viewBox='0 0 24 24'
     fill='none'
     xmlns='http://www.w3.org/2000/svg'
--- src/modules/common/components/Icons/Hamburger.tsx
+++ src/modules/common/components/Icons/Hamburger.tsx
@@ -2,9 +2,9 @@ import { SVGProps } from 'react';
 
 const SvgHamburger = (props: SVGProps<SVGSVGElement>) => (
   <svg
-    width='1em'
-    height='1em'
-    viewBox='0 0 24 24'
+    width={24}
+    height={24}
+
     fill='none'
     xmlns='http://www.w3.org/2000/svg'
     {...props}
